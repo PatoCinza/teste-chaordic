a04cfdfd884608317d32e63f6ab830660b1959d5
Gabriel Luis <gabriel.luis.cinza@gmail.com>
2017-08-31 11:31:14 -0300

    building HTML with JSON

--- js/main.js
+++ js/main.js
@@ -7,13 +7,45 @@ window.onload = function() {
 };
 
 function X(data) { //callback
-  var dataFromServer = data;
-  console.log(dataFromServer.data);
+    var reference = data.data.reference;
+    var recommendation = data.data.recommendation;
+    buildReference(reference);
+    buildRecomm(recommendation);
+}
+
+function buildRecomm(recomm) {
+    var txt = "";
+    for (var i = 0; i < recomm.length; i++) {
+        txt += '<article>';
+        txt += '<img src="http:' + recomm[i].imageName + '" alt="reference">';
+        var name = (recomm[i].name);
+        txt += '<p>'+ (recomm[i].name).substring(0, 99) +'... </p>';
+        if(recomm[i].oldPrice)
+            txt += '<p> De: '+recomm[i].oldPrice+'</p>';
+        txt += '<p class="t-font-red">Por: '+recomm[i].price+ ' ' + recomm[i].productInfo.paymentConditions + '</p>';
+        txt += '</article>';
+    }
+
+    var element = document.getElementById("list").innerHTML = txt;
+}
+
+function buildReference(ref) {
+    var txt = '<article>';
+    txt += '<img src="http:' + ref.item.imageName + '" alt="reference">';
+    var name = (ref.item.name);
+    txt += '<p>'+ (ref.item.name).substring(0, 99) +'... </p>';
+    if(ref.item.oldPrice)
+        txt += '<p> De: '+ref.item.oldPrice+'</p>';
+    txt += '<p class="t-font-red">Por: '+ref.item.price+ ' ' + ref.item.productInfo.paymentConditions + '</p>';
+    txt += '</article>';
+
+    var element = document.getElementById("reference").innerHTML = txt;
+
 }
 
 function nextPage() {
-    var element = document.getElementById('list');
-    var currentPosition = parseFloat(element.style.left.replace(/[^0-9\.\-]+/g, ""));
+    var element = document.getElementById('list'); 
+    var currentPosition = parseFloat(element.style.transform.replace(/[^0-9\.\-]+/g, ""));
     var finalPosition = currentPosition + (-17.5);
     var id = setInterval(frame, 5);
     function frame() {
@@ -21,14 +53,16 @@ function nextPage() {
         clearInterval(id);
       } else {
         currentPosition--; 
-        element.style.left = currentPosition + 'vw'; 
+        element.style.transform = 'translateX(' + currentPosition + 'vw)'; 
+        element.style.WebkitTransform = 'translateX(' + currentPosition + 'vw)'; 
+        element.style.msTransform = 'translateX(' + currentPosition + 'vw)'; 
       }
     }
 }
 
 function previousPage() {
     var element = document.getElementById('list');
-    var currentPosition = parseFloat(element.style.left.replace(/[^0-9\.\-]+/g, ""));
+    var currentPosition = parseFloat(element.style.transform.replace(/[^0-9\.\-]+/g, ""));
     var finalPosition = currentPosition + (17.5);
     var id = setInterval(frame, 5);
     function frame() {
@@ -36,7 +70,9 @@ function previousPage() {
         clearInterval(id);
       } else {
         currentPosition++; 
-        element.style.left = currentPosition + 'vw'; 
+        element.style.transform = 'translateX(' + currentPosition + 'vw)'; 
+        element.style.WebkitTransform = 'translateX(' + currentPosition + 'vw)'; 
+        element.style.msTransform = 'translateX(' + currentPosition + 'vw)'; 
       }
     }
 }
